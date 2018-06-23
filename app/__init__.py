e5e92e4207f6a30c9a2d24d2a2de4f6c130bcdd3
Marcos Stefani da Silva Rosa <marcos.cantor@gmail.com>
2018-06-23 00:40:17 -0300

    My first Page with Sucuri

---
+++ app/__init__.py
@@ -0,0 +1,18 @@
+import os
+from flask import Flask, render_template, send_from_directory
+from sucuri import rendering
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    param = { 'body': rendering.template('app/main.suc') }
+    return render_template('index.html', **param)
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+if __name__ == "__main__":
+    app.run()
