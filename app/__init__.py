35c2b2481e6a6e530777e616abb6d94f136fe34c
Marcos Rosa <marcos.cantor@gmail.com>
2018-07-05 17:47:38 -0300

    add favicon

--- app/__init__.py
+++ app/__init__.py
@@ -9,10 +9,10 @@ def index():
     param = { 'body': rendering.template('app/main.suc') }
     return render_template('index.html', **param)
 
-# @app.route('/favicon.ico')
-# def favicon():
-#     return send_from_directory(os.path.join(app.root_path, 'static'),
-#                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == "__main__":
     app.run()
